937bfe7f486e975297996071082d09058876db66
Rishabh Singh <140543813+777Rishabh777@users.noreply.github.com>
2025-08-13 09:51:40 +0530

    Add files via upload

---
+++ 12-08-2025/loops.py
@@ -0,0 +1,91 @@
+#loop control statement
+#break , continue and pass
+
+# fruits= ['apple', "banana", "cherry", "kiwi", "mango"]
+# for x in fruits:
+#     if x == "banana" :
+#         print(x)
+#         break
+
+# def sum_number(n):
+#     sum = 0
+#     for i in range(1,n+1):
+#         sum = sum + i
+#     print(sum)
+
+# sum_number(9)
+
+# def sum_even_numbers(n):
+#     sum = 0
+#     for i in range(2,n+1,2):
+#         sum = sum + i
+#     print(sum)
+
+# sum_even_numbers(5) 
+
+# def count_vowels(word):
+#     vowels = "Rishabh"
+#     count=0
+#     for i in word:
+#         if i in vowels:
+#             count= count+1
+#     print(count)
+
+# count_vowels("cauliflower")
+
+# def list_square(list1):
+#     list2 = []
+#     for i in list1:
+#         list2.append(i*i)
+#     print(list2)
+
+# list_square([2,5,7,8])
+
+# def reverse_string(str):
+#     rev_string = ""
+#     for i in str:
+#         rev_string = i + rev_string
+#     print(rev_string)
+
+
+# (reverse_string("python"))
+
+# print(len("hello"))
+
+# def words_length(list):
+#     for i in list:
+#         print(len(i))
+
+# list=["apple", "Banana", "cherry"]
+# words_length(list)
+
+# while loop 
+# while condition:
+#     Statement 1
+#     Statement 2
+#     Statement 3
+
+#basic loop example
+# count = 1
+# while count <=5:
+#     print(count)
+#     count+=1
+
+# for i in range(5):
+#     if i == 3:
+#         break
+#     print(i)
+# else:
+#     print("Loops finished successfully")
+
+# def factorial(n):
+#     if n<0:
+#         return
+#     elif n==0:
+#         return 1
+#     else:
+#         result = 1
+#         for in range(i,n+1):
+#             result*=iw
+
+#OOPs
