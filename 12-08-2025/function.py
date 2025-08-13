266c66c7ba47509eb006746f333ef7bb787d8ad3
Rishabh Singh <140543813+777Rishabh777@users.noreply.github.com>
2025-08-13 09:54:13 +0530

    Add files via upload

---
+++ 12-08-2025/function.py
@@ -0,0 +1,26 @@
+#def python():
+    #print(L)
+    #print(O)
+    #print(V)
+    #print(E)
+    #print(P)
+    #print(Y)
+    #print(T)
+    #print(H)
+    #print(O)
+    #print(N)
+    #python()
+    #python()
+    #python()
+    #python()
+    #python()
+
+#def add(a,b): #arguments
+#    print(a+b)
+
+#add(4,5) #parameter
+
+def greet_user(name):
+    print("welcome",name)
+
+greet_user("rahul")
